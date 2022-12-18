70fd210410cc13481171535ee2786441a8150d0b
Thomas Hartmann <thomas.hartmann@th-ht.de>
2022-12-18 14:15:35 +0100

    use proper gettext (#19)

--- wagtail_simple_gallery/models.py
+++ wagtail_simple_gallery/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
